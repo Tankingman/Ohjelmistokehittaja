f65bb063b09764ae098a0d0a66ea02a4ba113ee3
Tankingman <82443847+Tankingman@users.noreply.github.com>
2021-10-26 10:32:30 +0300

    Add files via upload

---
+++ Harjoitukset.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace C_harjoitukset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            int luku1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syotä toinen luku: ");
+            int luku2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("X = " + (luku1 + luku2));
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            int luku3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syotä toinen luku: ");
+            int luku4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("X = " + (luku3 - luku4));
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            int luku5 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syotä toinen luku: ");
+            int luku6 = int.Parse(Console.ReadLine());
+            Console.WriteLine("X = " + (luku5 * luku6));
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            int luku7 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syotä toinen luku: ");
+            int luku8 = int.Parse(Console.ReadLine());
+            Console.WriteLine("X = " + (luku7 / luku8));
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            int luku9 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syotä toinen luku: ");
+            int luku10 = int.Parse(Console.ReadLine());
+            Console.WriteLine("X = " + (luku9 % luku10));
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            int luku11 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syotä toinen luku: ");
+            int luku12 = int.Parse(Console.ReadLine());
+            Console.WriteLine("X = " + (luku11 += luku12));
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            int luku13 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syotä toinen luku: ");
+            int luku14 = int.Parse(Console.ReadLine());
+            Console.WriteLine("X = " + (luku13 -= luku14));
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            int luku15 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syotä toinen luku: ");
+            int luku16 = int.Parse(Console.ReadLine());
+            Console.WriteLine("X = " + (luku15 *= luku16));
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            int luku17 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syotä toinen luku: ");
+            int luku18 = int.Parse(Console.ReadLine());
+            Console.WriteLine("X = " + (luku17 /= luku18));
+
+        }
+    }
+}
