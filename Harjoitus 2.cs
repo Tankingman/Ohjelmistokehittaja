97451377b48b10419794b62371939251ec07bd12
Tankingman <82443847+Tankingman@users.noreply.github.com>
2021-10-27 09:50:11 +0300

    Add files via upload

---
+++ Harjoitus 2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Harjoituksua_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna ensimmäinen kokonaisluku: ");
+
+            int first1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Anna toinen kokonaisluku: ");
+
+            int second2 = int.Parse(Console.ReadLine());
+
+            var max = (first1 > second2) ? first1 : second2;
+            var min = (second2 < first1) ? second2: first1;
+            Console.WriteLine("luvut suuruusjärjestyksessä: " + max + " , " + min );
+        }
+    }
+}
